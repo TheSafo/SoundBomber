c5c027b1c62ff9cae64bb7119ee3339adb3ea316
Jake Saferstein <JakeSaf@Jakes-MacBook-Pro-3.local>
2015-04-06 19:54:58 -0700

    Initial Commit

---
+++ SoundBomber/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SoundBomber
+//
+//  Created by Jake Saferstein on 4/6/15.
+//  Copyright (c) 2015 Jake Saferstein. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
