0d65d5eb866c735d8d43bb5bcc4cce83f49df8de
Jake Saferstein <JakeSaf@Jakes-MacBook-Pro-3.local>
2015-05-08 22:47:53 -0700

    READY FOR V1 (also added restore purchases button)

--- SoundBomber/RemoveAdsViewController.m
+++ SoundBomber/RemoveAdsViewController.m
@@ -12,6 +12,7 @@
 @interface RemoveAdsViewController ()
 
 @property (nonatomic) UIButton* purchButton;
+@property (nonatomic) UIButton* restorePurchase;
 
 @end
 
@@ -25,13 +26,29 @@
         int h = self.view.frame.size.height;
         
         _purchButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-        _purchButton.frame = CGRectMake(w/2 - 75, h/2 - 30, 150, 60);
+        _purchButton.frame = CGRectMake(w/2 - 75, h/2 - 100, 150, 60);
         [_purchButton addTarget:self action:@selector(purchasePressed) forControlEvents:UIControlEventTouchUpInside];
         [_purchButton setTitle:@"Remove Ads" forState:UIControlStateNormal];
         [_purchButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-        _purchButton.backgroundColor = [UIColor colorWithRed:59.0/255.0 green:89.0/255.0 blue:152.0/255.0 alpha:1]; //FB blue//[UIColor colorWithRed:149.0/255.0 green:205.0/255.0 blue:222.0/255.0 alpha:1];
+        _purchButton.backgroundColor = [UIColor colorWithRed:59.0/255.0 green:89.0/255.0 blue:152.0/255.0 alpha:1]; //FB blue
         _purchButton.layer.cornerRadius = 5;
         _purchButton.layer.borderWidth = 2;
+        
+
+        _restorePurchase = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        _restorePurchase.frame = CGRectMake(w/2 - 75, h/2 - 30, 150, 60);
+        [_restorePurchase addTarget:self action:@selector(restorePressed) forControlEvents:UIControlEventTouchUpInside];
+        [_restorePurchase setTitle:@"Restore Purchases" forState:UIControlStateNormal];
+        [_restorePurchase setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        _restorePurchase.backgroundColor = [UIColor colorWithRed:59.0/255.0 green:89.0/255.0 blue:152.0/255.0 alpha:1]; //FB blue
+        _restorePurchase.layer.cornerRadius = 5;
+        _restorePurchase.layer.borderWidth = 2;
+        
+        if(ADS_ON) {
+            [self.view addSubview:_purchButton];
+        }
+        [self.view addSubview:_restorePurchase];
+
         
         
         self.view.backgroundColor = [UIColor colorWithRed:149.0/255.0 green:205.0/255.0 blue:222.0/255.0 alpha:1];
@@ -39,35 +56,16 @@
     return self;
 }
 
-//    self.tabBar.barTintColor = [UIColor colorWithRed:149.0/255.0 green:205.0/255.0 blue:222.0/255.0 alpha:1];
-
-
 -(void) purchasePressed
 {
     [[StoreManager sharedInstance] purchaseAds];
 }
 
--(void)viewWillAppear:(BOOL)animated
+-(void) restorePressed
 {
-    if(!ADS_ON)
-    {
-        int w = self.view.frame.size.width;
-        int h = self.view.frame.size.height;
-        
-        UILabel* lbl = [[UILabel alloc] initWithFrame:CGRectMake(w/2 - 75, h/2-30, 150, 60)];
-        lbl.backgroundColor = [UIColor colorWithRed:59.0/255.0 green:89.0/255.0 blue:152.0/255.0 alpha:1]; //FB blue
-        lbl.text = @"To remove this view restart the app.";
-        lbl.numberOfLines = 2;
-        lbl.minimumScaleFactor = .5;
-        lbl.textAlignment = NSTextAlignmentCenter;
-        lbl.layer.borderWidth = 2;
-        lbl.layer.cornerRadius = 5;
-        [self.view addSubview:lbl];
-    }
-    else {
-        
-        [self.view addSubview:_purchButton];
-    }
+    [[StoreManager sharedInstance] restorePurchase];
 }
+
+
 
 @end
